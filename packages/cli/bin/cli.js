9a4a819de28e6bf03d574d04141f15b108481118
Xiang Zhang <12361249@qq.com>
2023-04-19 13:57:37 +0800

    update cli

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -6,61 +6,82 @@ import {Command} from 'commander';
 import chalk from "chalk";
 import inquirer from "inquirer";
 import ora from "ora";
-import {execSync, exec} from "child_process";
+import {exec, execSync} from "child_process";
 
 const program = new Command();
 
 const prompt = (projectName) => {
-  const promptList = [
-    {
-      type: 'input',
-      message: 'Please input project name:',
-      name: 'name',
-      default: projectName,
-      validate(val) {
-        if (val.trim() === '') {
-          return 'Project name is required!';
+    const promptList = [
+        {
+            type: 'input',
+            message: 'Please input project name:',
+            name: 'name',
+            default: projectName,
+            validate(val) {
+                if (val.trim() === '') {
+                    return 'Project name is required!';
+                }
+                return true;
+            }
         }
-        return true;
-      }
-    }
-  ]
-  inquirer.prompt(promptList).then(res => {
-    downloadGit(res.name)
-  })
+    ]
+    inquirer.prompt(promptList).then(res => {
+        downloadGit(res.name)
+    })
 }
 
 const downloadGit = (projectName) => {
-  let spinner = ora('downloading template...');
-  spinner.start();
-  exec(`git clone https://github.com/congeer/minigame-template.git ${projectName}`,
-      (err, stdout, stderr) => {
-        if (err) {
-          spinner.fail();
-          console.log(chalk.red(err));
-          return;
-        }
-        execSync(`rm -rf ${projectName}/.git`);
-        const packageString = execSync(`cat ${projectName}/package.json`).toString();
-        const packageJson = JSON.parse(packageString);
-        packageJson.name = projectName;
-        execSync(`echo '${JSON.stringify(packageJson, null, 4)}' > ${projectName}/package.json`);
-        execSync(`cd ${projectName} && git init && git add . && git commit -m "init"`);
-        spinner.succeed();
-        console.log(chalk.green('Project initialization finished!'));
-      })
+    let spinner = ora('downloading template...');
+    spinner.start();
+    exec(`git clone https://github.com/congeer/minigame-template.git ${projectName}`,
+        (err, stdout, stderr) => {
+            if (err) {
+                spinner.fail();
+                console.log(chalk.red(err));
+                return;
+            }
+            execSync(`rm -rf ${projectName}/.git`);
+            const packageString = execSync(`cat ${projectName}/package.json`).toString();
+            const packageJson = JSON.parse(packageString);
+            packageJson.name = projectName;
+            execSync(`echo '${JSON.stringify(packageJson, null, 4)}' > ${projectName}/package.json`);
+            execSync(`cd ${projectName} && git init && git add . && git commit -m "init"`);
+            spinner.succeed();
+            console.log(chalk.green('Project initialization finished!'));
+        })
 }
-program.name('zero-cli')
+program.name('minigame')
 
 
-program.command("init [projectName]").action((projectName) => {
-  prompt(projectName)
+program.command("init [projectName]").description("init a new project").action((projectName) => {
+    prompt(projectName)
 })
 
-
-program.on('--help', () => {
-  console.log('Commands:');
-  console.log('  init [projectName]  init a new project');
+program.command("update [version]").description("update project version").action((version) => {
+    if (!version) {
+        console.log("Please input version")
+        return;
+    }
+    let packageString;
+    try {
+        packageString = execSync(`cat ./package.json`).toString();
+    } catch (e) {
+        console.log("Can't find package.json")
+        return;
+    }
+    const packageJson = JSON.parse(packageString);
+    for (let dependenciesKey in packageJson.dependencies) {
+        if (dependenciesKey.startsWith("@minigame/")) {
+            packageJson.dependencies[dependenciesKey] = version;
+        }
+    }
+    for (let devDependenciesKey in packageJson.devDependencies) {
+        if (devDependenciesKey.startsWith("@minigame/")) {
+            packageJson.devDependencies[devDependenciesKey] = version;
+        }
+    }
+    execSync(`echo '${JSON.stringify(packageJson, null, 2)}' > ./package.json`);
+    console.log("Update success");
 })
 
 program.parse(process.argv)
